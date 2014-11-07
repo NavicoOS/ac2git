09917cc4634a600b8335afbd1630ab342db442d1
Lazar Sumar <lazar.sumar@navico.com>
2014-11-07 16:08:27 +1300

    Drafting up the main accurev2git script's configuration options.

--- accurev2git.py
+++ accurev2git.py
@@ -10,18 +10,196 @@
 # ################################################################################################ #
 
 import sys
+import argparse
+import os
+import xml.etree.ElementTree as ElementTree
+
 import accurev
 
 # ################################################################################################ #
 # Script Classes                                                                                   #
 # ################################################################################################ #
+class Config(object):
+    class AccuRev(object):
+        @classmethod
+        def fromxmlelement(cls, xmlElement):
+            if xmlElement is not None and xmlElement.tag == 'accurev':
+                depot    = xmlElement.attrib.get('depot')
+                username = xmlElement.attrib.get('username')
+                password = xmlElement.attrib.get('password')
+                
+                return cls(depot, username, password)
+            else:
+                return None
+            
+        def __init__(self, depot, username = None, password = None):
+            self.depot    = depot
+            self.username = username
+            self.password = password
     
+        def __repr__(self):
+            str = "Config.AccuRev(depot=" + repr(self.depot)
+            str += ", username="          + repr(self.username)
+            str += ", password="          + repr(self.password)
+            str += ")"
+            
+            return str
+            
+    class Git(object):
+        @classmethod
+        def fromxmlelement(cls, xmlElement):
+            if xmlElement is not None and xmlElement.tag == 'git':
+                repoPath = xmlElement.attrib.get('repo-path')
+                
+                return cls(repoPath)
+            else:
+                return None
+            
+        def __init__(self, repoPath):
+            self.repoPath = repoPath
 
+        def __repr__(self):
+            str = "Config.Git(repoPath=" + repr(self.repoPath)
+            str += ")"
+            
+            return str
+            
+    class UserMap(object):
+        @classmethod
+        def fromxmlelement(cls, xmlElement):
+            if xmlElement is not None and xmlElement.tag == 'map-user':
+                accurevUsername = xmlElement.attrib.get('accurev-username')
+                gitName         = xmlElement.attrib.get('git-name')
+                gitEmail        = xmlElement.attrib.get('git-email')
+                
+                return cls(accurevUsername, gitName, gitEmail)
+            else:
+                return None
+            
+        def __init__(self, accurevUsername, gitName, gitEmail):
+            self.accurevUsername = accurevUsername
+            self.gitName         = gitName
+            self.gitEmail        = gitEmail
+    
+        def __repr__(self):
+            str = "Config.UserMap(accurevUsername=" + repr(self.accurevUsername)
+            str += ", gitName="                     + repr(self.gitName)
+            str += ", gitEmail="                    + repr(self.gitEmail)
+            str += ")"
+            
+            return str
+            
+    @staticmethod
+    def FilenameFromScriptName(scriptName):
+        (root, ext) = os.path.splitext(scriptName)
+        return root + '.config'
+
+    @classmethod
+    def fromxmlstring(cls, xmlString):
+        # Load the XML
+        xmlRoot = ElementTree.fromstring(xmlString)
+        
+        if xmlRoot is not None and xmlRoot.tag == "accurev2git":
+            accurev = Config.AccuRev.fromxmlelement(xmlRoot.find('accurev'))
+            git     = Config.Git.fromxmlelement(xmlRoot.find('git'))
+            
+            usermaps = []
+            userMapsElem = xmlRoot.find('usermaps')
+            if userMapsElem is not None:
+                for userMapElem in userMapsElem.findall('map-user'):
+                    usermaps.append(Config.UserMap.fromxmlelement(userMapElem))
+            
+            return cls(accurev, git, usermaps)
+        else:
+            # Invalid XML for an accurev2git configuration file.
+            return None
+
+    def __init__(self, accurev = None, git = None, usermaps = None):
+        self.accurev  = accurev
+        self.git      = git
+        self.usermaps = usermaps
+        
+    def __repr__(self):
+        str = "Config(accurev=" + repr(self.accurev)
+        str += ", git="         + repr(self.git)
+        str += ", usermaps="    + repr(self.usermaps)
+        str += ")"
+        
+        return str
+
+# ################################################################################################ #
+# Script Functions                                                                                 #
+# ################################################################################################ #
+def DumpExampleConfigFile(outputFilename):
+    exampleContents = """
+<accurev2git>
+    <accurev username="joe_bloggs" password="joanna" depot="Trunk" />
+    <git repo-path="/put/the/git/repo/here" />
+    <usermaps>
+        <map-user accurev-username="joe_bloggs" git-name="Joe Bloggs" git-email="joe@bloggs.com" />
+    </usermaps>
+</accurev2git>
+"""
+    file = open(outputFilename, 'w')
+    file.write(exampleContents)
+    file.close()
+    
+    
 # ################################################################################################ #
 # Script Main                                                                                      #
 # ################################################################################################ #
 def AccuRev2GitMain(argv):
-    pass
+    # Try and load the config file
+    doesConfigExist = True
+    
+    configFilename = Config.FilenameFromScriptName(argv[0])
+    configXml = None
+    try:
+        configFile = open(configFilename)
+        configXml = configFile.read()
+        configFile.close()
+    except:
+        doesConfigExist = False
+        
+    config = None
+    if configXml is not None:
+        config = Config.fromxmlstring(configXml)
+
+    if config is None:
+        config = Config(Config.AccuRev(), Config.Git(), [])
+    
+    # Set-up and parse the command line arguments. Examples from https://docs.python.org/dev/library/argparse.html
+    parser = argparse.ArgumentParser(description="Conversion tool for migrating AccuRev repositories into Git")
+    parser.add_argument('--accurev-username', nargs='?', dest='accurevUsername', default=config.accurev.username)
+    parser.add_argument('--accurev-password', nargs='?', dest='accurevPassword', default=config.accurev.password)
+    parser.add_argument('--accurev-depot',    nargs='?', dest='accurevDepot',    default=config.accurev.depot)
+    parser.add_argument('--dump-example-config', nargs='?', dest='exampleConfigFilename', const='no-filename', default=None)
+    
+    args = parser.parse_args()
+    
+    # Dump example config if specified
+    if args.exampleConfigFilename is not None:
+        if args.exampleConfigFilename == 'no-filename':
+            exampleConfigFilename = configFilename + '.example'
+        else:
+            exampleConfigFilename = args.exampleConfigFilename
+        
+        DumpExampleConfigFile(exampleConfigFilename)
+    
+    # Set the overrides for in the configuration from the arguments
+    config.accurev.username = args.accurevUsername
+    config.accurev.password = args.accurevPassword
+    config.accurev.depot    = args.accurevDepot
+    
+    if config.accurev.username is None or config.accurev.password is None:
+        print "No AccuRev username or password specified."
+        return 1
+
+    accurev.Login(config.accurev.username, config.accurev.password)
+    
+    print accurev.History(depot=config.accurev.depot, timeSpec="now.3")
+    
+    accurev.Logout()
 
 # ################################################################################################ #
 # Script Start                                                                                     #
